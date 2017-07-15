3d1aaff556a2fa5194085ed25dd04fc68168c47b
yuanye <yuanye_ptr@qq.com>
2017-07-15 14:02:49 +0800

    gofmt queens.go

--- eight-queens/queens.go
+++ eight-queens/queens.go
@@ -17,7 +17,7 @@ func queens(SIZE, nQueen, cols, diags, r_diags uint64) uint64 {
 		diag := uint64(1 << (nQueen + i))
 		r_diag := uint64(1 << (SIZE - 1 - nQueen + i))
 
-		if col&cols==0 && diag&diags==0 && r_diag&r_diags==0 {
+		if col&cols == 0 && diag&diags == 0 && r_diag&r_diags == 0 {
 			nSolutions += queens(SIZE, nQueen+1, cols|col, diags|diag, r_diags|r_diag)
 		}
 	}
@@ -29,16 +29,16 @@ func main() {
 		fmt.Println("Usage: ./", os.Args[0], "<queen size>")
 		return
 	}
-  // ParseUint(s string, base int, bitSize int)
-  SIZE,err := strconv.ParseUint(os.Args[1], 10, 64)
-  if err != nil {
-    fmt.Println("Usage: ./", os.Args[0], "<queen size>")
+	// ParseUint(s string, base int, bitSize int)
+	SIZE, err := strconv.ParseUint(os.Args[1], 10, 64)
+	if err != nil {
+		fmt.Println("Usage: ./", os.Args[0], "<queen size>")
 		return
-  }
+	}
 
 	start_time := time.Now()
 	// nSolution := queens(strconv.Atoi(os.Args[1]), 0, 0, 0, 0)
-  nSolution := queens(SIZE, 0, 0, 0, 0)
+	nSolution := queens(SIZE, 0, 0, 0, 0)
 	fmt.Println("Solution num = ", nSolution)
 	fmt.Println("elapsed time = ", time.Since(start_time))
 }
