e4659d8fcdbea6dfa16f02884bd93d7780e43e5c
yuanye <yuanye_ptr@qq.com>
2017-07-15 13:47:34 +0800

    Add Golang solution

---
+++ eight-queens/queens.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+)
+
+func queens(SIZE, nQueen, cols, diags, r_diags uint64) uint64 {
+	if nQueen == SIZE {
+		return 1
+	}
+	var nSolutions uint64
+	for i := uint64(0); i < SIZE; i++ {
+		col := uint64(1 << i)
+		diag := uint64(1 << (nQueen + i))
+		r_diag := uint64(1 << (SIZE - 1 - nQueen + i))
+
+		if col&cols==0 && diag&diags==0 && r_diag&r_diags==0 {
+			nSolutions += queens(SIZE, nQueen+1, cols|col, diags|diag, r_diags|r_diag)
+		}
+	}
+	return nSolutions
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: ./", os.Args[0], "<queen size>")
+		return
+	}
+  // ParseUint(s string, base int, bitSize int)
+  SIZE,err := strconv.ParseUint(os.Args[1], 10, 64)
+  if err != nil {
+    fmt.Println("Usage: ./", os.Args[0], "<queen size>")
+		return
+  }
+
+	start_time := time.Now()
+	// nSolution := queens(strconv.Atoi(os.Args[1]), 0, 0, 0, 0)
+  nSolution := queens(SIZE, 0, 0, 0, 0)
+	fmt.Println("Solution num = ", nSolution)
+	fmt.Println("elapsed time = ", time.Since(start_time))
+}
